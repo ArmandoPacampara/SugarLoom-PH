94f12937f2f41dc20d1fffb40223ccaae3ad7a0d
Armando <mandopacampara@gmail.com>
2026-05-16 15:00:22 +0800

    feat: clear guest cart upon login and registration for privacy

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -111,6 +111,9 @@ class RegisteredUserController extends Controller
 
         session()->forget('pending_registration');
 
+        // Clear guest cart items upon registration/login
+        session()->forget(['cart', 'promo_code']);
+
         event(new Registered($user));
 
         Auth::login($user);
